f62d9321dc7e232f1eda85b4483835d95262aa0e
WHQ <2283149823@qq.com>
2022-05-13 13:22:52 +0800

    Update transformer.py

--- downstream/transformer.py
+++ downstream/transformer.py
@@ -1,3 +1,7 @@
+import os
+from itertools import zip_longest
+import math
+import pandas as pd
 import torch
 import torch.nn as nn
 import numpy as np
@@ -5,15 +9,40 @@ from torch import optim
 from sklearn.utils import shuffle
 import torch.utils.data as Data
 
+from dataset import DataSetRaw
 from utils import next_batch
 
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 d_model = 512  # Embedding Size
 d_ff = 2048  # FeedForward dimension
 d_k = d_v = 64  # dimension of K(=Q), V
-# n_layers = 6  # number of Encoder of Decoder Layer
-# n_heads = 8  # number of heads in Multi-Head Attention
-src_vocab_size = 865
-tgt_vocab_size = 865
+n_layers = 6  # number of Encoder of Decoder Layer
+n_heads = 8  # number of heads in Multi-Head Attention
+src_vocab_size = 100000
+tgt_vocab_size = 100000
+device = 'cuda:0'
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=100000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        '''
+        x: [seq_len, batch_size, d_model]
+        '''
+        temp = self.pe[:x.size(0), :]
+        x = x + temp
+        return self.dropout(x)
 
 
 def get_attn_pad_mask(seq_q, seq_k):
@@ -60,13 +89,12 @@ class ScaledDotProductAttention(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, n_heads):
+    def __init__(self):
         super(MultiHeadAttention, self).__init__()
-        self.n_heads = n_heads
-        self.W_Q = nn.Linear(d_model, d_k * self.n_heads, bias=False)
-        self.W_K = nn.Linear(d_model, d_k * self.n_heads, bias=False)
-        self.W_V = nn.Linear(d_model, d_v * self.n_heads, bias=False)
-        self.fc = nn.Linear(self.n_heads * d_v, d_model, bias=False)
+        self.W_Q = nn.Linear(d_model, d_k * n_heads, bias=False)
+        self.W_K = nn.Linear(d_model, d_k * n_heads, bias=False)
+        self.W_V = nn.Linear(d_model, d_v * n_heads, bias=False)
+        self.fc = nn.Linear(n_heads * d_v, d_model, bias=False)
 
     def forward(self, input_Q, input_K, input_V, attn_mask):
         '''
@@ -77,20 +105,18 @@ class MultiHeadAttention(nn.Module):
         '''
         residual, batch_size = input_Q, input_Q.size(0)
         # (B, S, D) -proj-> (B, S, D_new) -split-> (B, S, H, W) -trans-> (B, H, S, W)
-        Q = self.W_Q(input_Q).view(batch_size, -1, self.n_heads, d_k).transpose(1,
-                                                                                2)  # Q: [batch_size, n_heads, len_q, d_k]
-        K = self.W_K(input_K).view(batch_size, -1, self.n_heads, d_k).transpose(1,
-                                                                                2)  # K: [batch_size, n_heads, len_k, d_k]
-        V = self.W_V(input_V).view(batch_size, -1, self.n_heads, d_v).transpose(1,
-                                                                                2)  # V: [batch_size, n_heads, len_v(=len_k), d_v]
+        Q = self.W_Q(input_Q).view(batch_size, -1, n_heads, d_k).transpose(1, 2)  # Q: [batch_size, n_heads, len_q, d_k]
+        K = self.W_K(input_K).view(batch_size, -1, n_heads, d_k).transpose(1, 2)  # K: [batch_size, n_heads, len_k, d_k]
+        V = self.W_V(input_V).view(batch_size, -1, n_heads, d_v).transpose(1,
+                                                                           2)  # V: [batch_size, n_heads, len_v(=len_k), d_v]
 
-        attn_mask = attn_mask.unsqueeze(1).repeat(1, self.n_heads, 1,
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1,
                                                   1)  # attn_mask : [batch_size, n_heads, seq_len, seq_len]
 
         # context: [batch_size, n_heads, len_q, d_v], attn: [batch_size, n_heads, len_q, len_k]
         context, attn = ScaledDotProductAttention()(Q, K, V, attn_mask)
         context = context.transpose(1, 2).reshape(batch_size, -1,
-                                                  self.n_heads * d_v)  # context: [batch_size, len_q, n_heads * d_v]
+                                                  n_heads * d_v)  # context: [batch_size, len_q, n_heads * d_v]
         output = self.fc(context)  # [batch_size, len_q, d_model]
         return nn.LayerNorm(d_model).cuda()(output + residual), attn
 
@@ -114,10 +140,9 @@ class PoswiseFeedForwardNet(nn.Module):
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, n_heads):
+    def __init__(self):
         super(EncoderLayer, self).__init__()
-        self.n_heads = n_heads
-        self.enc_self_attn = MultiHeadAttention(self.n_heads)
+        self.enc_self_attn = MultiHeadAttention()
         self.pos_ffn = PoswiseFeedForwardNet()
 
     def forward(self, enc_inputs, enc_self_attn_mask):
@@ -133,14 +158,11 @@ class EncoderLayer(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, embedding_layer, n_layers, n_heads):
+    def __init__(self):
         super(Encoder, self).__init__()
-        self.n_layers = n_layers
-        self.n_heads = n_heads
         self.src_emb = nn.Embedding(src_vocab_size, d_model)
-        # self.pos_emb = PositionalEncoding(d_model)
-        self.pos_emb = embedding_layer
-        self.layers = nn.ModuleList([EncoderLayer(self.n_heads) for _ in range(self.n_layers)])
+        self.pos_emb = PositionalEncoding(d_model)
+        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
 
     def forward(self, enc_inputs):
         '''
@@ -158,11 +180,10 @@ class Encoder(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, n_heads):
+    def __init__(self):
         super(DecoderLayer, self).__init__()
-        self.n_heads = n_heads
-        self.dec_self_attn = MultiHeadAttention(self.n_heads)
-        self.dec_enc_attn = MultiHeadAttention(self.n_heads)
+        self.dec_self_attn = MultiHeadAttention()
+        self.dec_enc_attn = MultiHeadAttention()
         self.pos_ffn = PoswiseFeedForwardNet()
 
     def forward(self, dec_inputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask):
@@ -181,14 +202,11 @@ class DecoderLayer(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, embedding_layer, n_layers, n_heads):
+    def __init__(self):
         super(Decoder, self).__init__()
-        self.n_layers = n_layers
-        self.n_heads = n_heads
         self.tgt_emb = nn.Embedding(tgt_vocab_size, d_model)
-        # self.pos_emb = PositionalEncoding(d_model)
-        self.pos_emb = embedding_layer
-        self.layers = nn.ModuleList([DecoderLayer(self.n_heads) for _ in range(self.n_layers)])
+        self.pos_emb = PositionalEncoding(d_model)
+        self.layers = nn.ModuleList([DecoderLayer() for _ in range(n_layers)])
 
     def forward(self, dec_inputs, enc_inputs, enc_outputs):
         '''
@@ -216,10 +234,10 @@ class Decoder(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, embedding_layer, n_layers, n_heads):
+    def __init__(self):
         super(Transformer, self).__init__()
-        self.encoder = Encoder(embedding_layer, n_layers, n_heads).cuda()
-        self.decoder = Decoder(embedding_layer, n_layers, n_heads).cuda()
+        self.encoder = Encoder().cuda()
+        self.decoder = Decoder().cuda()
         self.projection = nn.Linear(d_model, tgt_vocab_size, bias=False).cuda()
 
     def forward(self, enc_inputs, dec_inputs):
@@ -238,22 +256,39 @@ class Transformer(nn.Module):
         return dec_logits.view(-1, dec_logits.size(-1)), enc_self_attns, dec_self_attns, dec_enc_attns
 
 
-def train_transformer(dataset, model, num_epoch, batch_size, device):
-    model = model.cuda()
+def train_transformer(dataset, transformer, num_epoch, batch_size, device):
     traj_enc_inputs, traj_dec_inputs, traj_dec_outputs = zip(*dataset.gen_enc2dec())
     criterion = nn.CrossEntropyLoss(ignore_index=0)
-    optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.99)
+    optimizer = optim.SGD(transformer.parameters(), lr=1e-3, momentum=0.99)
     for epoch in range(num_epoch):
+        all_loss = 0
         for batch in next_batch(shuffle(list(zip(traj_enc_inputs, traj_dec_inputs, traj_dec_outputs))),
                                 batch_size=batch_size):
             # Value filled with num_loc stands for masked tokens that shouldn't be considered.
             enc_inputs, dec_inputs, dec_outputs = zip(*batch)
-            enc_inputs, dec_inputs, dec_outputs = enc_inputs.cuda(), dec_inputs.cuda(), dec_outputs.cuda()
+
+            enc_inputs = np.transpose(np.array(list(zip_longest(*enc_inputs, fillvalue=0))))
+            dec_inputs = np.transpose(np.array(list(zip_longest(*dec_inputs, fillvalue=0))))
+            dec_outputs = np.transpose(np.array(list(zip_longest(*dec_outputs, fillvalue=0))))
+            enc_inputs, dec_inputs, dec_outputs = torch.LongTensor(enc_inputs).to(device), torch.LongTensor(
+                dec_inputs).to(device), torch.LongTensor(dec_outputs).to(device)
+
             # outputs: [batch_size * tgt_len, tgt_vocab_size]
-            outputs, enc_self_attns, dec_self_attns, dec_enc_attns = model(enc_inputs, dec_inputs)
-            loss = criterion(outputs, dec_outputs.view(-1))
-            print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+            outputs, enc_self_attns, dec_self_attns, dec_enc_attns = transformer(enc_inputs, dec_inputs)
+            loss = criterion(outputs, dec_outputs.contiguous().view(-1))
+
+            all_loss += loss
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+        print('-------Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(all_loss))
+
+
+if __name__ == '__main__':
+    file_name = "test_data"
+    raw_df = pd.read_hdf(os.path.join('../data/h5_data', file_name + ".h5"), key='data')
+    dataset = DataSetRaw(raw_df)
+    transformer = Transformer().to(device)
+    train_transformer(dataset, transformer=transformer, num_epoch=100, batch_size=32, device=device)
